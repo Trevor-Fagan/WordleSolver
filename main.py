1a4e70868f3345f342139e269b4302e1b5ea3d9e
Trevor Fagan <trevorfagan77@gmail.com>
2022-02-20 17:13:24 -0500

    Fixed most of the logic in shortening list

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ opening_guess = "crane"
 
 in_the_ans = []
 in_correct_place = [" ", " ", " ", " ", " "]
+letter_bank = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # Initialize Wordle
 with open('list_of_words.txt','r') as f:
@@ -38,15 +39,41 @@ def Guess(guess, num_guess):
     return [result, num_guess]
 
 # Solving Wordle
-def Solve(response):
-    pass
+def NextGuess(res):
+    global list_of_words
+    next_guess = ""
+
+    if num_guesses < 2:
+        return opening_guess
+
+    ## THIS AREA IN PROGRESS ##
+    itr = 0
+    for key in res[0]: # for each letter in dictionary
+        if res[0][key] == "Green": # If its in the right place
+            for word in list_of_words:
+                if word[itr] != key:
+                    list_of_words.remove(word)
+        itr += 1
+
+    ## IN PROGRESS END ##
+
+    if len(list_of_words) > 1:
+        next_guess = list_of_words[random.randint(0, len(list_of_words) - 1)]
+    else:
+        next_guess = list_of_words[0]
+
+    return next_guess
+
+
 
 # Main Wordle Code
+res = ""
 while user_guess != the_word and num_guesses < 6:
-    user_guess = input("Guess: ")
+    my_next_guess = NextGuess(res)
+    print(len(list_of_words), my_next_guess)
+    user_guess = NextGuess(res)
 
     res = Guess(user_guess, num_guesses)
-    print(res[0])
     num_guesses = res[1]
 
 if found:
